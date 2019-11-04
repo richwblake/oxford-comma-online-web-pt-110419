7975b2440c1f96246898167a3cfee741634ad74c
Wills Blake <richwblake@gmail.com>
2019-11-04 19:14:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  formatted_string = String.new
+  if array.size == 1
+    formatted_string << array[0]
+  elsif array.size == 2
+    formatted_string = array.join( " and " )
+  else
+    array.each_with_index { |word, index| 
+      index == array.size - 1 ? formatted_string << ", and #{word}"
+    }
+  end
+  
+  formatted_string
 end
